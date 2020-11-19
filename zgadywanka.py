1d6dada7e2c9f6079fd7c34660c8311aa6cbfb5a
Myszexx <michalr550@gmail.com>
2020-11-19 23:18:47 +0100

    Update zgadywanka.py

--- zgadywanka.py
+++ zgadywanka.py
@@ -1,19 +1,14 @@
 import random
-print("Witaj w zgadywance, została wyloswana liczba od 1 - 10, masz 4 szanse na zgadniecie co to za liczba(Wpsuj tylko liczby!)")
-input("Gotowy?")
+print("Witaj w zgadywance, została wyloswana liczba od 1 - 10")
 los = random.randrange(1,11)
 for i in range(1,5):
     strzał = int(input('Podaj twój strzał: '))
-    if strzał < los and strzał in range(0,11):
+    if strzał < los and strzał:
         print("Szukana liczba jest większa od podanej!")
-    elif strzał > los and strzał in range(0,11):
+    elif strzał > los and strzał:
         print("Szukana liczba jest mniejsza od podanej!")
     elif strzał == los:
         str(i)
         str(los)
         print("Gratuluje! To było",los,"Zgadłeś za",i,"próbą")
-        exit()
-    else:
-        print("Miales podac liczbe od 1 - 10 -próba :) ")
-print("Niestety nie zdgadles :(")
     
